499d59af44f5a144455e37a3e6fec17d8ee7dedf
Md. Nazmul Hasan <mdnazmul612@gmail.com>
2022-05-20 16:10:32 +0600

    Schema and graphql server created

--- Server/index.js
+++ Server/index.js
@@ -1,9 +1,13 @@
 const express = require('express');
-
+const { graphqlHTTP } = require('express-graphql');
+const { schema } = require('./Schemas/schema');
 const app = express();
 const PORT = 5000;
 
-
+app.use('/graphql',graphqlHTTP({
+    schema: schema,
+    graphiql: true
+}))
 app.listen(PORT, ()=>{
     console.log(`Server is running at port ${PORT}`);
 })
