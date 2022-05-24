f48ccc323ea3f81e110364a52b5f67517198d075
Md. Nazmul Hasan <mdnazmul612@gmail.com>
2022-05-24 15:33:11 +0600

    Commit

--- Server/Schemas/TypeDefs/BookType.js
+++ Server/Schemas/TypeDefs/BookType.js
@@ -1,12 +1,34 @@
-const { GraphQLObjectType, GraphQLInt, GraphQLString} = require('graphql');
+const { GraphQLObjectType, GraphQLInt, GraphQLString, GraphQLID, GraphQLList} = require('graphql');
+const { books, authors } = require('../../Data/Data');
+
 
 const BookType = new GraphQLObjectType({
     name:'Book',
     fields: ()=>({
-        id: { type: GraphQLInt },
+        id: { type: GraphQLID },
         name: { type: GraphQLString },
-        genre: { type: GraphQLString }
+        genre: { type: GraphQLString },
+        author: {
+            type: AuthorType,
+            resolve(parent,args){
+                return authors.find((x)=> x.id == parent.authorId)
+            }
+        }
+    })
+})
+const AuthorType = new GraphQLObjectType({
+    name:'Author',
+    fields: ()=>({
+        id: { type: GraphQLID },
+        name: { type: GraphQLString },
+        age: { type: GraphQLInt },
+        books:{
+            type: new GraphQLList(BookType),
+            resolve(parent,args){
+                return books.filter((x)=> x.authorId == parent.id)
+            }
+        }
     })
 })
 
-module.export = { BookType }
+module.exports = { BookType, AuthorType }
